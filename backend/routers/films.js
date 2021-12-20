9780153c00de1fdf6d6aa92976fda0c3c0caebcf
davidz1995 <55600188+davidz1995@users.noreply.github.com>
2021-12-20 18:12:27 -0300

    Creado buscador de peliculas por personaje

--- backend/routers/films.js
+++ backend/routers/films.js
@@ -4,9 +4,11 @@ const axios = require('axios')
 const {allFilms} = require ('../utils/constants')
 const {allCharacters} = require ('../utils/constants')
 const { Film } = require ('../models/films')
+const { Character } = require ('../models/characters')
 
 router.get('/',async(req, res) => {
     let filmsDB = await Film.find();
+    let charactersDB = await Character.find();
     if(filmsDB) {
         let film = filmsDB[0].result.result
         res.send(film)
@@ -24,6 +26,42 @@ router.get('/',async(req, res) => {
         }
     }
 })
+
+router.get('/characterByFilm/:id',async(req, res) => {
+    let id = req.params.id
+    let filmsDB = await Film.find();
+    let charactersDB = await Character.find();
+    let acc = []
+    try{
+        let searchedCharacter = charactersDB[0].result.results.filter(e => e.uid === id)
+        let selectedURL = searchedCharacter[0].url
+        let listOfCharactersByFilm = filmsDB[0].result.result.map(e => {
+            let obj = {
+                id: e.uid,
+                title: e.properties.title,
+                characters: e.properties.characters
+            }
+            return obj
+            })
+        let filmDes = listOfCharactersByFilm.map(e => {
+            let aux = e.characters
+            aux.unshift(e.title)
+            return aux
+        })
+        
+        for (let item of filmDes){
+           if(item.some(e => e === selectedURL)){
+               acc.push(item[0])
+           }
+        }
+        
+        res.send(acc)
+    } catch (error) {
+        res.send(error)
+    }
+    })
+
+
 
 router.get('/:character',async(req, res) => {
     let id = req.params.character
